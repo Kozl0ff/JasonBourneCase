fac1c5f8c611c2c401dff2e598f3195eaa6dab53
Levshunovma <levshunovma@gmail.com>
2020-10-01 17:09:29 +0400

    Delete running example on start and fixed env syntax

--- docker/micro-cluster-lab/script_files/bootstrap.sh
+++ docker/micro-cluster-lab/script_files/bootstrap.sh
@@ -6,9 +6,6 @@ if [ "$HOSTNAME" = node-master ]; then
     start-dfs.sh
     start-yarn.sh
     #start-master.sh
-    cd /root/lab
-    /opt/hadoop/bin/hadoop fs -put /spark_volume/spark_example.txt /spark_example.txt
-    /opt/spark/bin/spark-submit --class Example --master yarn /spark_volume/java-spark-1.0-SNAPSHOT.jar /spark_example.txt
 fi
 #bash
 while :; do :; done & kill -STOP $! && wait $!
