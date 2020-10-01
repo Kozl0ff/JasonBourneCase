3a9c86fa5dbbf68d28d380bff4d542f063dee22f
Mikhail Levshunov <Levshunovma@gmail.com>
2020-10-01 17:31:59 +0400

    Add docker files (#6)
    * Add docker-compose files
    
    * Add files needed to build spark docker container

---
+++ docker/micro-cluster-lab/script_files/bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+hdfs namenode -format
+service ssh start
+if [ "$HOSTNAME" = node-master ]; then
+    start-dfs.sh
+    start-yarn.sh
+    #start-master.sh
+fi
+#bash
+while :; do :; done & kill -STOP $! && wait $!
