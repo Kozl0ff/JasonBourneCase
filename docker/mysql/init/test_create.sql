67a381a16803e8da87ad2214a5ddf6ed33768dae
Kozl0ff <nik.k200181@gmail.com>
2020-10-06 12:38:12 +0400

    Add congifuration files for Grafana

--- docker/mysql/init/test_create.sql
+++ docker/mysql/init/test_create.sql
@@ -1,3 +1,3 @@
-CREATE TABLE test(id INT NOT NULL, value INT NOT NULL, data DATE NOT NULL);
+CREATE TABLE test(id INT NOT NULL, value INT NOT NULL, data DATETIME NOT NULL);
 CREATE USER 'grafanaReader' IDENTIFIED BY 'password';
 GRANT SELECT ON bdata.* TO 'grafanaReader';
